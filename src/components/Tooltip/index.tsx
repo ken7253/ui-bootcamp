297a3328f1c6a65df7679082678bd5a61ffbe29e
ken7253 <ken7253@gmail.com>
2023-10-17 20:45:53 +0900

    一時的にツールチップを隠す処理

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -1,4 +1,5 @@
-import { FC, useEffect, useId, useState } from 'react';
+import type { FC } from 'react';
+import { useEffect, useId, useState } from 'react';
 
 import styles from './style.module.css';
 import { Props } from './spec';
@@ -9,8 +10,19 @@ export const Tooltip: FC<Props> = (props) => {
   const id = useId();
 
   useEffect(() => {
-    // TODO: ESCで一時的に非表示にする処理
-  }, []);
+    const escapeHandler = (e: KeyboardEvent, state: boolean) => {
+      if (e.key !== 'Escape' || e.repeat) return;
+      setOpen(state);
+    };
+
+    const controller = new AbortController();
+    window.addEventListener('keydown', (e) => escapeHandler(e, false), { signal: controller.signal });
+    window.addEventListener('keyup', (e) => escapeHandler(e, true), { signal: controller.signal });
+
+    return () => {
+      controller.abort();
+    };
+  }, [open]);
 
   return (
     <div className={styles.container} onPointerEnter={() => setOpen(true)} onPointerLeave={() => setOpen(false)}>
