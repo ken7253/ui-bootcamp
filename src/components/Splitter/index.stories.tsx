3a07130322dc35d01f3f4b64b349a5fc9a26de37
ken7253 <ken7253@gmail.com>
2024-09-26 00:03:56 +0900

    設計を見直し

--- src/components/Splitter/index.stories.tsx
+++ src/components/Splitter/index.stories.tsx
@@ -6,18 +6,39 @@ type Stroy = Meta<typeof Splitter>;
 
 export default {
   title: 'Other/Splitter',
-  component: Splitter,
   parameters: {
     layout: 'fullscreen',
   },
 } satisfies Stroy;
 
-export const Default = {
-  args: {
-    direction: 'row',
-    children: [
-      <div style={{ width: '100%', minHeight: '500px', backgroundColor: 'red' }}>A</div>,
-      <div style={{ width: '100%', minHeight: '500px', backgroundColor: 'blue' }}>B</div>,
-    ],
+export const Row = {
+  render: () => {
+    return (
+      <Splitter.Container direction="row">
+        <Splitter.Item>
+          <div style={{ width: 500, height: 500, backgroundColor: 'red' }}>A</div>
+        </Splitter.Item>
+        <Splitter.Separator />
+        <Splitter.Item>
+          <div style={{ width: 500, height: 500, backgroundColor: 'blue' }}>B</div>
+        </Splitter.Item>
+      </Splitter.Container>
+    );
+  },
+} satisfies Stroy;
+
+export const Column = {
+  render: () => {
+    return (
+      <Splitter.Container direction="column">
+        <Splitter.Item>
+          <div style={{ width: 500, height: 500, backgroundColor: 'red' }}>A</div>
+        </Splitter.Item>
+        <Splitter.Separator />
+        <Splitter.Item>
+          <div style={{ width: 500, height: 500, backgroundColor: 'blue' }}>B</div>
+        </Splitter.Item>
+      </Splitter.Container>
+    );
   },
 } satisfies Stroy;
--- src/components/Splitter/index.tsx
+++ src/components/Splitter/index.tsx
@@ -1,20 +1,48 @@
-import type { FC, ReactNode } from 'react';
+import { createContext, useContext, type FC, type PropsWithChildren } from 'react';
 
 import styles from './style.module.css';
 
-type Props = {
-  direction: 'column' | 'row';
-  children: [ReactNode, ReactNode];
+const DirectionContext = createContext<Direction | null>(null);
+
+type Direction = 'column' | 'row';
+
+type ContainerProps = PropsWithChildren<{
+  direction: Direction;
+}>;
+
+const Container: FC<ContainerProps> = ({ direction, children }) => {
+  return (
+    <DirectionContext.Provider value={direction}>
+      <div style={{ flexDirection: direction }} className={styles.container}>
+        {children}
+      </div>
+    </DirectionContext.Provider>
+  );
 };
 
-export const Splitter: FC<Props> = (props: Props) => {
-  const { children, direction } = props;
+type SeparatorProps = {
+  size?: number;
+};
+
+const Separator: FC<SeparatorProps> = ({ size = 5 }) => {
+  const direction = useContext(DirectionContext);
 
   return (
-    <div className={styles.container} style={{ flexDirection: direction }}>
-      <div className={styles.content}>{children[0]}</div>
-      <div className={styles.separator} tabIndex={0} />
-      <div className={styles.content}>{children[1]}</div>
-    </div>
+    <div
+      style={{ width: direction === 'row' ? size : 0, height: direction === 'column' ? size : 0 }}
+      className={styles.separator}
+    />
   );
+};
+
+type ItemProps = PropsWithChildren;
+
+const Item: FC<ItemProps> = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export const Splitter = {
+  Container,
+  Separator,
+  Item,
 };
